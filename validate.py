ce50c9bc35d4a8bab8d32c54a1a80821fd356de7
Maksim Yegorov <Maksim_Yegorov@intuit.com>
2016-12-13 16:22:16 -0800

    add 'ownership' field to schedule E and schedule K1

--- validate.py
+++ validate.py
@@ -11,18 +11,31 @@ schema_path = os.path.join(abs_path, 'schema.json')
 with open(schema_path) as schema_file:
     schema = json.load(schema_file)
     resolver = RefResolver('file://' + abs_path + '/', schema)
+    validator = Draft4Validator(schema, resolver = resolver)
 
-    for f in glob('./data/*/*.json'):
+    for f in glob('./data/valid/*.json'):
         with open(f) as sample_file:
             sample = sample_file.read()
 
             try:
-                validator = Draft4Validator(schema, resolver = resolver)
                 errors = sorted(validator.iter_errors(json.loads(sample)),
                         key=str)
                 for error in errors:
                     print("file: %s:" %f)
                     print("trace:", error.message)
                     print("path:", list(error.path),'\n')
+            except ValidationError as err:
+                print(err.message)
+
+    for f in glob('./data/invalid/*.json'):
+        with open(f) as sample_file:
+            sample = sample_file.read()
+
+            try:
+                errors = sorted(validator.iter_errors(json.loads(sample)),
+                        key=str)
+                assert len(errors) > 0, \
+                    "Expected to find a ValidationError in file %s, but found none..." %f
+
             except ValidationError as err:
                 print(err.message)
